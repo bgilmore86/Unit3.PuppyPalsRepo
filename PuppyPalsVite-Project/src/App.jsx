9a7a8cfe6e6a612b2666d721835d3aea2507232a
Barb T <stayyounng86@gmail.com>
2024-01-09 15:41:08 -0600

    update 4 App.jsx

--- PuppyPalsVite-Project/src/App.jsx
+++ PuppyPalsVite-Project/src/App.jsx
@@ -5,15 +5,21 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppyList: ", setPuppies);
-
+  console.log(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log("featPupId: ", setPuppies);
 
   return (
       <div className="App">
     {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return <p onClick={() =>{console.log("puppy id: ", puppy.id)}} key={puppy.id}>{puppy.name}</p>;
         })
+    }
+    {
+       puppies.map((puppy) => {
+        return <p>{puppy.name}</p>
+      })
     }
         </div>
      );
