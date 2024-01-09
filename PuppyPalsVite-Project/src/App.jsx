944f6c51e3f297f806e65313e66f4f943195e2c1
Barb T <stayyounng86@gmail.com>
2024-01-09 16:20:14 -0600

    update 5 App.jsx

--- PuppyPalsVite-Project/src/App.jsx
+++ PuppyPalsVite-Project/src/App.jsx
@@ -5,24 +5,36 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-  console.log("featPupId: ", setPuppies);
+  console.log(puppyList, setPuppies);
+  
+  const [featuredPup, setFeaturedPup] = useState(null);
 
   return (
-      <div className="App">
+    
+      <div>
     {
         puppies.map((puppy) => {
-          return <p onClick={() =>{console.log("puppy id: ", puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+          return <p onClick = {()=>{
+            setFeaturedPup(puppy)
+            setFeatPupId(puppy.id)}
+          } key={puppy.id}>{puppy.name}</p>;
         })
     }
-    {
-       puppies.map((puppy) => {
-        return <p>{puppy.name}</p>
-      })
-    }
+
+{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
         </div>
-     );
-  }
+      )}
+    
+  )
+
+</div>
+  )}
 
 export default App
